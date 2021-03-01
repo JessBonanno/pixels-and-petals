ce1a3da8730711c5f1b507faf58ce959d15daec3
Jess <jessbonanno@gmail.com>
2021-03-01 16:07:23 -0500

    blocking image uplaod

--- pages/image-upload/index.js
+++ pages/image-upload/index.js
@@ -9,16 +9,21 @@ import {parseCookies} from '../../services/parseCookies';
 
 
 const ImageUpload = ({token}) => {
-  const {admin} = jwt.decode(token);
-  console.log(admin);
+
+  const [admin, setAdmin] = useState(null);
+
 
   const [selectedImage, setSelectedImage] = useState();
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-
-
-  }, [images]);
+  if (token) {
+    const decoded = jwt.decode(token)
+    if (decoded.admin) {
+      setAdmin(decoded.admin)
+    }
+  }
+  }, [token]);
 
   const {mutate: uploadImage} = useMutate({
     verb: 'POST',
@@ -33,7 +38,7 @@ const ImageUpload = ({token}) => {
   const handleUpload = async () => {
     const formData = new FormData();
     formData.append('image', selectedImage);
-    formData.append('token', localStorage.getItem('token'))
+    formData.append('token', localStorage.getItem('token'));
     try {
       const response = await uploadImage(formData);
       setImages([
@@ -45,53 +50,62 @@ const ImageUpload = ({token}) => {
     }
   };
 
-  return (
-    <div className={styles.uploadContainer}>
-      <div style={{
-        display: 'flex',
-        margin: 'auto',
-        width: 400,
-        flexWrap: 'wrap',
-      }}>
-        <h3>Upload new image</h3> <br/>
-      </div>
-      <input
-        accept="image/*"
-        id="icon-button-file"
-        type="file"
-        style={{display: 'none'}}
-        onChange={handleChange}
-      />
-      <label htmlFor="icon-button-file">
-        <h5>Choose Image</h5>
+  if (admin) {
+
+    return (
+      <div className={styles.uploadContainer}>
+        <div style={{
+          display: 'flex',
+          margin: 'auto',
+          width: 400,
+          flexWrap: 'wrap',
+        }}>
+          <h3>Upload new image</h3> <br/>
+        </div>
+        <input
+          accept="image/*"
+          id="icon-button-file"
+          type="file"
+          style={{display: 'none'}}
+          onChange={handleChange}
+        />
+        <label htmlFor="icon-button-file">
+          <h5>Choose Image</h5>
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span">
+            <FontAwesomeIcon
+              icon={['far', 'file-image']}/>
+          </IconButton>
+        </label>
+        <h5>Upload Image</h5>
         <IconButton
+          disabled={!selectedImage}
           color="primary"
           aria-label="upload picture"
-          component="span">
+          component="span"
+          onClick={handleUpload}
+        >
           <FontAwesomeIcon
-            icon={['far', 'file-image']}/>
+            icon={['fas', 'cloud-upload-alt']}/>
         </IconButton>
-      </label>
-      <h5>Upload Image</h5>
-      <IconButton
-        disabled={!selectedImage}
-        color="primary"
-        aria-label="upload picture"
-        component="span"
-        onClick={handleUpload}
-      >
-        <FontAwesomeIcon
-          icon={['fas', 'cloud-upload-alt']}/>
-      </IconButton>
-      <div className={styles.images}>
-        {images && images.map((image, key) => {
-          return (
-            <img key={key} src={image.url} alt={'uploaded image'}/>
-          );
-        })}
+        <div className={styles.images}>
+          {images && images.map((image, key) => {
+            return (
+              <img key={key} src={image.url} alt={'uploaded image'}/>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>Unauthorized</div>
+    );
+  }
+
+
 };
 
 
@@ -99,7 +113,7 @@ ImageUpload.getInitialProps = ({req}) => {
   const cookies = parseCookies(req);
   return {
     token: cookies.token
-  }
+  };
 
-}
-export default  ImageUpload;
+};
+export default ImageUpload;
