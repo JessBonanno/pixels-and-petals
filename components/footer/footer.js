78227a208cf1f1e3166c0bda38631d9aab34c251
Jess <jessbonanno@gmail.com>
2021-03-02 22:45:46 -0500

    finished basic landing layout

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import styles from './footer.module.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import styles from './footer.module.css';
 
 const Footer = () => {
   return (
     <div className={styles.footerContainer}>
       <h2>email@Gmail.com</h2>
-      <div className={styles.nav}>
+      <FontAwesomeIcon
+        icon={['fab', 'instagram']}
+        className={styles.icon}/>
+      <div className={styles.navContainer}>
+        <a
+          className={styles.navLink}>
+          Home
+        </a>
+        <a
+          className={styles.navLink}>
 
+          About
+        </a>
+        <a
+          className={styles.navLink}>
+
+          Gallery
+        </a>
+        <a
+          className={styles.navLink}>
+
+          Testimonial
+        </a>
+        <a
+          className={styles.navLink}>
+
+          Contact
+        </a>
       </div>
     </div>
   );
