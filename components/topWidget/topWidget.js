619bdf5fe7da8018a012c405f11a7addf1854be7
Jess <jessbonanno@gmail.com>
2021-03-05 16:11:14 -0500

    fixed style import

--- components/topWidget/topWidget.js
+++ components/topWidget/topWidget.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './topWidget.module.css';
+import styles from './topWIdget.module.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 
