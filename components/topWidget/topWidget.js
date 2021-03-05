7eb365e3b16ec154b1a6d2dba17e3cfe213dc02f
Jess <jessbonanno@gmail.com>
2021-03-05 16:14:40 -0500

    adjusted stats padding

--- components/topWidget/topWidget.js
+++ components/topWidget/topWidget.js
@@ -6,7 +6,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 const TopWidget = () => {
   return (
     <div className={styles.topWidgetContainer}>
-      <a href={'#home'} className={styles.widget}>Back to top <FontAwesomeIcon icon={['fas', 'arrow-up']} className={styles.arrow}/></a>
+      <a href={'#home'} className={styles.widget}>To Top <FontAwesomeIcon icon={['fas', 'arrow-up']} className={styles.arrow}/></a>
     </div>
   );
 };
