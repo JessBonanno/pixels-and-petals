3e3743f255e2fe5922daf160ee3f888356139dfc
Jess <jessbonanno@gmail.com>
2021-03-05 23:08:46 -0500

    fully responsive home page

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -5,10 +5,14 @@ import styles from './footer.module.css';
 const Footer = () => {
   return (
     <div className={styles.footerContainer} id={'footer'}>
-      <h2>email@Gmail.com</h2>
-      <FontAwesomeIcon
-        icon={['fab', 'instagram']}
-        className={styles.icon}/>
+      <h2>PixelsNPetals@Gmail.com</h2>
+      <a
+        href={'https://www.instagram.com/pixels.petals/'}
+        target={'_blank'}>
+        <FontAwesomeIcon
+          icon={['fab', 'instagram']}
+          className={styles.icon}/>
+      </a>
       <div className={styles.navContainer}>
         <a
           href={'#home'}
@@ -16,28 +20,28 @@ const Footer = () => {
           Home
         </a>
         <a
-                    href={'#about'}
+          href={'#about'}
 
           className={styles.navLink}>
 
           About
         </a>
         <a
-                    href={'#gallery'}
+          href={'#gallery'}
 
           className={styles.navLink}>
 
           Gallery
         </a>
         <a
-                    href={'#testimonial'}
+          href={'#testimonial'}
 
           className={styles.navLink}>
 
           Testimonial
         </a>
         <a
-                    href={'#footer'}
+          href={'#footer'}
 
           className={styles.navLink}>
 
--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home ({folders}) {
         <Gallery folders={folders}/>
         {/*<Testimonial/>*/}
         <TopWidget/>
-      {/*<Footer/>*/}
+      <Footer/>
     </div>
   );
 }
