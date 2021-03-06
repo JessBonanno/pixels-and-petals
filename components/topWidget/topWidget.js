c85d38d22db2fbbb135b0d1210532200fb2f5202
Jess <jessbonanno@gmail.com>
2021-03-05 23:11:34 -0500

    removed words from top button

--- components/topWidget/topWidget.js
+++ components/topWidget/topWidget.js
@@ -6,7 +6,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 const TopWidget = () => {
   return (
     <div className={styles.topWidgetContainer}>
-      <a href={'#home'} className={styles.widget}><span className={styles.text}>To Top</span><FontAwesomeIcon icon={['fas', 'arrow-up']} className={styles.arrow}/></a>
+      <a href={'#home'} className={styles.widget}><FontAwesomeIcon icon={['fas', 'arrow-up']} className={styles.arrow}/></a>
     </div>
   );
 };
